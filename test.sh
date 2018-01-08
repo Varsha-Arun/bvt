e2e200907f999069eb8dca1dbc54763b176305d0
Avi Cavale <avi@shippable.com>
2018-01-08 09:05:13 -0800

    testing automation

--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 export RES_KEYS=$1
+
 export RES_KEYS_UP=$(shipctl to_uppercase $RES_KEYS)
 echo $RES_KEYS_UP
 
-echo $(eval echo "$"$RES_KEYS_UP"_INTEGRATION_TEST")
-echo $(shipctl get_integration_resource_field $RES_KEYS "TEST")
+echo $(eval echo "$"$RES_KEYS_UP"_7")
+#echo $(shipctl get_integration_resource_field $RES_KEYS "TEST")
 
 main() {
   #setup test comparison values
