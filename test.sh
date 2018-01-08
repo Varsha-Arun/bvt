6060be6681c7095ccd0314ac79310b7365405752
Avi Cavale <avi@shippable.com>
2018-01-08 09:11:53 -0800

    testing automation

--- test.sh
+++ test.sh
@@ -1,22 +1,10 @@
 #!/bin/bash -e
-export RES_KEYS=$1
-
-export RES_KEYS_UP=$(shipctl to_uppercase $RES_KEYS)
-echo $RES_KEYS_UP
-
-echo BOO
-
-echo $(eval echo "$"$RES_KEYS_UP"_6")
-echo $(eval echo "$"$RES_KEYS_UP"_7")
-#echo $(shipctl get_integration_resource_field $RES_KEYS "TEST")
-
-printenv
 
 main() {
   #setup test comparison values
   . testParams.env
   npm install
-#  npm run start-tests
+  npm run start-tests
 }
 
-#main
+main
